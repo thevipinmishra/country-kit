561cf5d6c82f944aff86a604c55062cfd8b546a2
Vipin Mishra <thevipinmishra@gmail.com>
2024-12-28 12:33:29 +0530

    docs: add tailwind typography plugin and custom mono font, write docs

--- apps/docs/tailwind.config.mjs
+++ apps/docs/tailwind.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-            container: {
-                center: true,
-                padding: '1.5rem',
-            },
-            fontFamily: {
-                sans: ['Space Grotesk Variable', 'sans-serif'],
-            },
-        },
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+      },
+      fontFamily: {
+        sans: ["Space Grotesk Variable", "sans-serif"],
+        mono: ["Space Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
